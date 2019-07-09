f5d4236340c3f9b0a675ff27ca5d781ad34fa5a1
unknown <4bysswalk3r404@gmail.com>
2019-07-09 18:40:08 -0500

    Buffer overflow management

---
+++ py/lib/BufferSplit.py
@@ -0,0 +1,22 @@
+import sys
+
+def Vectorize(data, buffer=126):
+    dataVec = []
+    charArr = []
+    while data:
+        if sys.getsizeof("".join(charArr).encode()) < buffer:
+            charArr.append(data[-1])
+        if sys.getsizeof("".join(charArr).encode()) >= buffer:
+            charArr = charArr[:-1]
+            dataVec.append("".join(charArr).encode())
+            charArr = []
+        else:
+            data = data[:-1]
+    if charArr:
+        dataVec.append("".join(charArr).encode())
+    return dataVec
+
+if __name__ == "__main__":
+    mult = int(input("mult: "))
+    data = 'a'*mult
+    print(Vectorize(data))
