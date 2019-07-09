c13644ec74c6be260eb9664c697688d3eb4a3257
unknown <4bysswalk3r404@gmail.com>
2019-07-09 17:16:21 -0500

    fixing foundation

--- py/low/client.py
+++ py/low/client.py
@@ -1,14 +1,20 @@
-import socket
 import sys
+sys.path.append("..\\lib")
+sys.path.append(".\\lib")
+
+from get_lan_ip import get_lan_ip
+import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 HOST = input("Server ip: ")
+PORT = 3702
+if HOST == "":
+    HOST = "192.168.86.168"
 
-client.connect((HOST, 3702))
+client.connect((HOST, PORT))
 
 while 1:
     data = input(">>>")
-    #data_arr = []
-    div = sys.getsizeof(data) // 2024
+    #div = sys.getsizeof(data) // 126
     client.send(data.encode())
--- py/low/server.py
+++ py/low/server.py
@@ -1,12 +1,20 @@
+import sys
+sys.path.append("..\\lib")
+sys.path.append(".\\lib")
+
+from get_lan_ip import get_lan_ip
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind(('localhost', 135))
+HOST = get_lan_ip()
+PORT = 3702
+
+server.bind((HOST, PORT))
 server.listen(1)
 
 conn, addr = server.accept()
 
 while 1:
-    data = conn.recv(2024).decode()
+    data = conn.recv(126).decode()
     print(data)
