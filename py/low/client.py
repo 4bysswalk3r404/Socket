cee5d3d97dea6c9708ed747f1c31620f79aab4a6
unknown <4bysswalk3r404@gmail.com>
2019-07-09 21:02:48 -0500

    missed imports

--- py/low/client.py
+++ py/low/client.py
@@ -1,18 +1,20 @@
 import sys
 if sys.platform == 'linux':
     sys.path.append("../lib")
+    sys.path.append("./lib")
 elif sys.platform == 'win32':
-    sys.path.append("..\\lib")
+    sys.path.append("../lib")
+    sys.path.append(".\\lib")
 
 import os
 import socket
 import BufferSplit
+import caps
+from sending import *
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 HOST = input("Server ip: ")
 PORT = 3702
-
 client.connect((HOST, PORT))
 
 protocol = 'string'
--- py/low/server.py
+++ py/low/server.py
@@ -1,11 +1,15 @@
 import sys
 if sys.platform == 'linux':
     sys.path.append("../lib")
+    sys.path.append("./lib")
 elif sys.platform == 'win32':
     sys.path.append("..\\lib")
+    sys.path.append("./lib")
 
 import os
 import socket
+import caps
+from recieving import *
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = input("HOST: ")
