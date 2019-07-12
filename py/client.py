7957c8133065aee1735fb9cf255af4935bd4e93b
4bysswalk3r404 <4bysswalk3r404@gmail.com>
2019-07-12 14:31:33 -0500

    rearanging

---
+++ py/client.py
@@ -0,0 +1,33 @@
+import sys
+if sys.platform == 'linux':
+    sys.path.append("../lib")
+    sys.path.append("./lib")
+elif sys.platform == 'win32':
+    sys.path.append("../lib")
+    sys.path.append(".\\lib")
+
+import os
+import socket
+import BufferSplit
+import caps
+from sending import *
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = input("Server ip: ")
+PORT = 3702
+client.connect((HOST, PORT))
+
+protocol = 'string'
+while 1:
+    data = input('(%s)' % protocol)
+    command = data.split(' ')
+    if command[0] == 'use':
+        if command[1] == 'string':
+            protocol = 'string'
+        elif command[1] == 'file':
+            protocol = 'file'
+    else:
+        if protocol == 'string':
+            SendString(client, data)
+        elif protocol == 'file':
+            SendFile(client, data)
---
+++ py/server.py
@@ -0,0 +1,27 @@
+import sys
+if sys.platform == 'linux':
+    sys.path.append("./lib")
+elif sys.platform == 'win32':
+    sys.path.append("./lib")
+
+import os
+import socket
+import caps
+from recieving import *
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+HOST = input("HOST: ")
+PORT = 3702
+server.bind((HOST, PORT))
+server.listen(1)
+conn, addr = server.accept()
+print("connected to ", addr)
+
+while 1:
+    data = conn.recv(32).decode()
+    if '(&s)' in data:
+        RecieveString(conn)
+    elif '(&f)' in data:
+        RecieveFile(conn)
+    if '(!!)' in data:
+        break
