32f0abff797a2d5131efdbd2294b41386010c072
unknown <4bysswalk3r404@gmail.com>
2019-07-27 05:25:58 -0500

    #

--- py/lib/encrypt.py
+++ py/lib/encrypt.py
@@ -7,10 +7,12 @@ def encrypt(buffer, seed, datatype=str):
     if datatype is str:
         for c in buffer:
             encrypted.append(chr((ord(c) + random.randrange(256)) % 256))
+        ''.join(encrypted).encode()
     elif datatype is bytes:
         for c in buffer:
             encrypted.append(chr((c + random.randrange(256)) % 256))
-    return ''.join(encrypted).encode()
+        ''.join(encrypted)
+    return encrypted
 
 def decrypt(buffer, seed, datatype=str):
     random.seed(seed)
